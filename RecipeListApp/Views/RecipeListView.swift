91f7dc70c98cdd02ba215532c0f87e22345bf808
Milos Pavlovic <milospavlovicdev@gmail.com>
2023-03-10 18:32:15 +0100

    Changed Swift Ui List to Scroll View and LazyVstak for sorting

--- RecipeListApp/Views/RecipeListView.swift
+++ RecipeListApp/Views/RecipeListView.swift
@@ -12,24 +12,39 @@ struct RecipeListView: View {
    @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-    
+        
         NavigationView {
-            List(model.recipes) { r in
+            
+            VStack(alignment: .leading) {
                 
-                NavigationLink {
-                RecipeDetailView(recipe: r)
-                } label: {
-                    HStack(spacing: 20.0) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+                Text("All Recipes")
+                    .bold()
+                    
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink {
+                                RecipeDetailView(recipe: r)
+                            } label: {
+                                HStack(spacing: 20.0) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name).foregroundColor(.black)
+                                }
+                            }
+                        }
                     }
                 }
-            }.navigationTitle("All Recipes")
+            }
+            .padding(.leading)
         }
     }
 }
